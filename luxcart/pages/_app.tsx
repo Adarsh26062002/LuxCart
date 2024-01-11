b5187af9374834b448e5cce137d2a252979d6ac5
Adarsh Singh Parihar <adarshsinghparihar@ec2-23-23-0-216.compute-1.amazonaws.com>
2024-01-12 02:31:35 +0530

    Admin Navbar is created

--- luxcart/pages/_app.tsx
+++ luxcart/pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Poppins } from 'next/font/google'
 import { SessionProvider } from "next-auth/react"
+import Header from '@/components/Header'
 
 const inter = Poppins({ subsets: ['latin'], weight:'400'})
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider>
+      <Header/>
     <main className={`${inter.className}`}>
       <Component {...pageProps} />
     </main>
