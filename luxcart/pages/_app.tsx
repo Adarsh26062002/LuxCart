cc5536485c705df46373fb10d31d9ca9498fdff0
Adarsh Singh Parihar <adarshsinghparihar@ec2-23-23-0-216.compute-1.amazonaws.com>
2024-01-12 01:19:37 +0530

    Admin page authentication done

--- luxcart/pages/_app.tsx
+++ luxcart/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Poppins } from 'next/font/google'
+import { SessionProvider } from "next-auth/react"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const inter = Poppins({ subsets: ['latin'], weight:'400'})
+
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider>
+    <main className={`${inter.className}`}>
+      <Component {...pageProps} />
+    </main>
+    </SessionProvider>
+  )
 }
